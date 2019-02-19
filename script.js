2f8e264d238c877208c1ce969786939f5be1d69f
Ivaylo Plashkov <n3k0ne@gmail.com>
2019-02-19 15:44:54 +0200

    Fixed a bug in the transitioning of the "Go to top arrow"

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 $(document).ready(function(){
     var offset = 400;
     var duration =500;
-    
+    $('.to-top').fadeOut(0);
+
     $(window).scroll(function(){
      if($(window).scrollTop() >= offset){
-     $('.to-top').fadeIn(1000);
+     $('.to-top').fadeIn(duration);
     }else{
      $('.to-top').fadeOut(duration);
     }
