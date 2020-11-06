35e363b51b08e346bffede235a6f98119a23d5f9
Ivaylo Plashkov <ivaylo.plashkov@sap.com>
2020-11-06 18:31:12 +0200

    refactoring: fix identation

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-
-$(document).ready(function(){
-       
+$(document).ready(function() {
     $(this).scrollTop(0);
-   
-    var offset = 400;
-    var duration =1000;
+
+    var iOffset = 400,
+        iDuration = 1000;
     $('.to-top').fadeOut(0);
 
     $(window).scroll(function(){
-     if($(window).scrollTop() >= offset){
-     $('.to-top').fadeIn(duration);
-    }else{
-     $('.to-top').fadeOut(duration);
-    }
+        if ($(window).scrollTop() >= iOffset) {
+            $('.to-top').fadeIn(iDuration);
+        } else {
+            $('.to-top').fadeOut(iDuration);
+        }
     });
 });
